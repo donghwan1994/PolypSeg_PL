466a7160a6717547bf43bbbb243cb4f287ec9cf4
Hwangdonghwan <donghwan1994@kangwon.ac.kr>
2022-05-16 10:19:49 +0000

    Update train.py

--- train.py
+++ train.py
@@ -5,11 +5,10 @@ from torch.utils.data import DataLoader
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, ModelSummary
-from dataloader.dataset import PolypDataset
+from dataloader.dataset import PolypDataset, collate_resize
 
 from pl_lib import *
-from utils.custom_transforms import Resize, RandomResize, RandomHorizontalFlip, \
-    RandomVerticalFlip, RandomRotate, RandomRotate90, ToTensor, Normalize
+from utils.custom_transforms import *
 
 
 def train(args):    
@@ -34,6 +33,7 @@ def train(args):
             Normalize([0.485, 0.456, 0.406],
                     [0.229, 0.224, 0.225])
         ]
+        collate_fn = None
     elif 'sanet' in str(args.method):
         hparams = {
             'channels': 64,
@@ -61,17 +61,18 @@ def train(args):
             Normalize([0.485, 0.456, 0.406],
                     [0.229, 0.224, 0.225])
         ]
+        collate_fn = None
     elif 'msnet' in str(args.method):
         hparams = {
             'channels': 64,
+            'coeff': 0.1, 
             'epoch': 50,
-            'batch_size': 16,
+            'batch_size': 64,
             'lr': 0.05,
             'momentum': 0.9,
             'weight_decay': 5e-4,
             'nesterov': True,
             'lr_gamma': 0.5,
-            'milestones': [64, 96],
             'grad_clip_val': None,
             'grad_clip_algorithm': 'norm',
             'amp_backend': 'native',
@@ -81,7 +82,6 @@ def train(args):
         model = MSNet(hparams)
         train_transforms = [
             Resize((352, 352)),
-            RandomResize([224, 256, 288, 320, 352]),
             RandomHorizontalFlip(),
             RandomVerticalFlip(),
             RandomRotate(degrees=10),
@@ -89,6 +89,7 @@ def train(args):
             Normalize([0.485, 0.456, 0.406],
                     [0.229, 0.224, 0.225])
         ]
+        collate_fn = collate_resize
     else:
         raise RuntimeError("The method " + str(args.method) + " is not supported.")
     
@@ -102,7 +103,7 @@ def train(args):
     train_dataset = PolypDataset(args.data_root, train=True, transforms=train_transforms, color_exchange=True)
     val_dataset = PolypDataset(args.data_root, train=False, transforms=val_transforms, dataname='test')
     train_loader = DataLoader(train_dataset, batch_size=hparams['batch_size'], shuffle=True,
-                             num_workers=args.num_workers, pin_memory=True)
+                             num_workers=args.num_workers, pin_memory=True, collate_fn=collate_fn)
     val_loader = DataLoader(val_dataset, batch_size=hparams['batch_size'], shuffle=False,
                             num_workers=args.num_workers, pin_memory=True)
 
@@ -123,7 +124,9 @@ def train(args):
         ],
         amp_backend=hparams['amp_backend'], 
         amp_level=hparams['amp_level'],
-        precision=hparams['precision']
+        precision=hparams['precision'],
+        detect_anomaly=True,
+        log_every_n_steps=20
     )
 
     trainer.fit(model, train_loader, val_loader)
